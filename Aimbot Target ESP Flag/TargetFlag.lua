424fd5d5c3f4b45780b9cf36862c0e28bf4dcaed
m0nsterJ <kmmnttr@gmail.com>
2023-04-22 14:24:08 +0200

    Update TargetFlag.lua
    - Minor overall changes

--- Aimbot Target ESP Flag/TargetFlag.lua
+++ Aimbot Target ESP Flag/TargetFlag.lua
@@ -22,21 +22,19 @@ local cache =
 }
 
 local function UpdateTarget(entity)
+    cache.current_target = nil;
     if entity:GetName() then
         cache.current_target = entity;
-    else
-        cache.current_target = nil;
     end
 end
 
 local function DrawESP(builder)
     local builderentity = builder:GetEntity();
+        builder:Color(0, 0, 0, 0);
+        builder:AddTextRight("");
     if cache.current_target and builderentity:GetIndex() == cache.current_target:GetIndex() then
         builder:Color(255, 0, 0, 255);
         builder:AddTextRight("T");
-    else
-        builder:Color(0, 0, 0, 0);
-        builder:AddTextRight("");
     end
 end
 
