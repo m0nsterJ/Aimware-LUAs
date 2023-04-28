508e3577ce79bfc30c2cabad61ec1cd190c66e92
m0nsterJ <kmmnttr@gmail.com>
2023-04-28 16:32:40 +0200

    Update 1.1
    - Reorganized several functions
    - Added local player check
    - Removed unused local

--- Freestanding/Freestanding.lua
+++ Freestanding/Freestanding.lua
@@ -1,4 +1,4 @@
-local local_version = "1.0"
+local local_version = "1.1"
 ---@diagnostic disable-next-line: undefined-global
 local local_script_name = GetScriptName()
 local github_version_url = "https://raw.githubusercontent.com/m0nsterJ/Aimware-LUAs/main/Freestanding/version.txt"
@@ -17,7 +17,6 @@ if local_version ~= tostring(github_version) then
 end
 
 local freestand_check_b = gui.Checkbox((gui.Reference("Ragebot", "Anti-Aim", "Condition")), "freestand_check_b", "Freestand", false)
-local freestand_key_b = gui.Keybox((gui.Reference("Ragebot", "Anti-Aim", "Condition")), "freestand_key_b", "Freestand Key", 0)
 freestand_check_b:SetDescription("Hide your head behind edges.")
 
 local antiaim_edges =
@@ -30,40 +29,40 @@ local antiaim_edges =
 
 local at_edge_state
 local at_edge_cache
+local local_player
 local cached = false
-local should_exec = false
 local cache = { }
 local edge_aa_values = { }
 
 local function freestand()
-    local lp = entities.GetLocalPlayer()
 
-    if lp:IsAlive() and lp ~= nil then
+    local_player = entities.GetLocalPlayer()
+
+    if not local_player ~= nil and not local_player:IsAlive() then
+        return
+    end
+
+    if not cached then
+        if gui.GetValue("rbot.master") then
+            cache = {gui.GetValue(antiaim_edges[1]), gui.GetValue(antiaim_edges[2]), gui.GetValue(antiaim_edges[3]), gui.GetValue(antiaim_edges[4])}
+            at_edge_cache = gui.GetValue("rbot.antiaim.condition.autodir.edges")
+            cached = true
+        end
+    end
+
+    if cached then
+        at_edge_state = at_edge_cache
+        edge_aa_values = cache
 
         if freestand_check_b:GetValue() then
-            if not cached then
-                cache = {gui.GetValue(antiaim_edges[1]), gui.GetValue(antiaim_edges[2]), gui.GetValue(antiaim_edges[3]), gui.GetValue(antiaim_edges[4])}
-                at_edge_cache = gui.GetValue("rbot.antiaim.condition.autodir.edges")
-                cached = true
-            end
+            at_edge_state = true
+            edge_aa_values = {90; 58; -90; 58}
         end
 
-        if freestand_key_b:GetValue() ~= 0 then
-            if input.IsButtonDown(freestand_key_b:GetValue()) then
-                at_edge_state = true
-                edge_aa_values = {90; 58; -90; 58}
-            else
-                at_edge_state = at_edge_cache
-                edge_aa_values = cache
-            end
-        end
-
-        if cached then
-            for i = 1, #antiaim_edges do
-                if gui.GetValue(antiaim_edges[i]) ~= edge_aa_values[i] then
-                    gui.SetValue("rbot.antiaim.condition.autodir.edges", at_edge_state)
-                    gui.SetValue(antiaim_edges[i], edge_aa_values[i])
-                end
+        for i = 1, #antiaim_edges do
+            if gui.GetValue(antiaim_edges[i]) ~= edge_aa_values[i] then
+                gui.SetValue("rbot.antiaim.condition.autodir.edges", at_edge_state)
+                gui.SetValue(antiaim_edges[i], edge_aa_values[i])
             end
         end
     end
